dd968d21c0b69b2cb2017e6124d829fcc9360340
Baur Urazalinov <burazalinov@signifyhealth.com>
2022-02-06 12:10:54 -0600

    Added one unittest

--- BasicExercises.Tests/Exercise4Tests.cs
+++ BasicExercises.Tests/Exercise4Tests.cs
@@ -2,6 +2,11 @@ using Moq;
 using System;
 using Xunit;
 
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+
 namespace BasicExercises.Tests
 {
     /// <summary>
@@ -25,7 +30,37 @@ namespace BasicExercises.Tests
         [Fact]
         public void Get_all_stock_items()
         {
-            throw new NotImplementedException();
+            // Arrange
+            var data = new List<StoreItem>
+            {
+                new StoreItem { Name = "Widget", Count = 1 },
+                new StoreItem { Name = "Juice Box", Count = 2 },
+            }.AsQueryable();
+
+            var mockRepository = new Mock<DbSet<StoreItem>>();
+            mockRepository.As<IQueryable<StoreItem>>().Setup(m => m.Provider).Returns(data.Provider);
+            mockRepository.As<IQueryable<StoreItem>>().Setup(m => m.Expression).Returns(data.Expression);
+            mockRepository.As<IQueryable<StoreItem>>().Setup(m => m.ElementType).Returns(data.ElementType);
+            mockRepository.As<IQueryable<StoreItem>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
+
+            var mockContext = new Mock<RepositoryContext>();
+            mockContext.Setup(m => m.theStore).Returns(mockRepository.Object);
+
+            var sut = new StoreService(mockContext.Object); // sut - system under test
+
+            // Act
+            var items = sut.GetAllItems();
+
+            // Assert
+            Assert.Equal(2, items.Count);
+            Assert.Equal("Juice Box", items[0].Name);
+            Assert.Equal(2, items[0].Count);
+            Assert.Equal("Widget", items[1].Name);
+            Assert.Equal(1, items[1].Count);
+
+            // NOTE! It was a surprise that the order in the data list is not the same as in the in-memory database,
+            //       looks like the order by name in the result data set.
+            // TODO - fix this!
         }
 
         [Fact]
